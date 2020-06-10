095241f529c6de8074e53d6bb58e38a297d872e1
Maximilian Bachl <maximilian.bachl@nt.tuwien.ac.at>
2020-06-10 18:25:53 +0200

    Finish results for offline learning

--- ns-allinone-3.30.1/ns-3.30.1/plot_df.py
+++ ns-allinone-3.30.1/ns-3.30.1/plot_df.py
@@ -26,6 +26,9 @@ units = {"bw": "Mbit/s", "delay": "ms"}
 
 
 for path in paths:
+	print("path", path)
+	if os.path.isdir(path):
+		continue
 	df = pd.read_csv(path, sep=";", header=0)
 
 	relevant_thing = "bw" if "bw" in path else "delay"
@@ -39,6 +42,7 @@ for path in paths:
 	path = path.replace("delay", full_names["delay"])
 
 	average_queue_length = df["average_queue_length"].tolist()
+	average_throughput = np.array(df["average_throughput"].tolist())/1000000*8
 	average_max_queue_length = df["average_max_queue_length"].tolist()
 	x = df[relevant_thing].tolist()
 
@@ -51,16 +55,29 @@ for path in paths:
 	# average_max_queue_length = windowize(df["average_max_queue_length"].tolist())
 	# x = windowize(df[relevant_thing].tolist())
 
-	plt.plot(x, average_queue_length, label="queue length")
-	plt.plot(x, average_max_queue_length, label="max queue length")
+	fig, ax1 = plt.subplots()
+
+	things = []
+	things += (ax1.plot(x, average_queue_length, label="queue length"))
+	things += (ax1.plot(x, average_max_queue_length, label="max queue length"))
+
+	ax2 = ax1.twinx()
+	things += (ax2.plot(x, average_throughput, label="throughput", color="red"))
+
 	appropriate_dir = "/".join(path.split("/")[:-1])+"/plots/"
 	os.makedirs(appropriate_dir, exist_ok=True)
 
 	file_name = ".".join(path.split("/")[-1].split(".")[:-1])+".pdf"
-	plt.grid(True)
-	plt.xlabel(f"{full_names[relevant_thing]} ({units[relevant_thing]})")
-	plt.ylabel("avg. queue size (packets)")
-	plt.legend()
-	plt.title(cc_name)
+	# plt.grid(True)
+	ax1.set_xlabel(f"{full_names[relevant_thing]} ({units[relevant_thing]})")
+	ax1.set_ylabel("queue size (packets)")
+	ax2.set_ylabel("throughput (Mbit/s)")
+	ax1.set_ylim(ymin=0)
+	ax2.set_ylim(ymin=0)
+	# ax1.legend(loc=0)
+	# ax2.legend(loc=1)
+	# print("things", things)
+	plt.legend(things, [l.get_label() for l in things], loc="lower right")
+	# plt.title(cc_name)
 	plt.savefig(appropriate_dir+file_name)
 	plt.close()
